854b0260369b00d932886ee5a78fa03bee917c43
PavithraCS25 <40050711+PavithraCS25@users.noreply.github.com>
2024-04-14 17:23:26 +0800

    Update chatmodel.py with comments

--- model/chatmodel.py
+++ model/chatmodel.py
@@ -1,5 +1,7 @@
+# Suppressing warnings to avoid cluttering the output
 import warnings
 warnings.filterwarnings('ignore')
+# Importing necessary libraries
 import os
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
@@ -9,20 +11,24 @@ from langchain_community.vectorstores import FAISS
 import boto3
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.chat_models import BedrockChat
-
+'''
+This code block contains code for the chat model built to answer questions from FWD, and Synapxe website
+'''
 
 # Create a session with your specific AWS profile
 session = boto3.Session(profile_name='myprofile')
+# Path to save the FAISS vector store
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
-
+# Function to initialize and configure the chat model
 def demo_chat():
-     
+     # Configuration parameters for the chat model
     mistral_inference_modifier = {'max_tokens':300, 
                       "temperature":0.2,
                       "top_p":0.5,
                       "top_k":20,
                    }
+     # Initializing the chat model with specified parameters
     llm = BedrockChat(credentials_profile_name="myprofile",
                           region_name="us-east-1",
                         model_id= "mistral.mistral-7b-instruct-v0:2",
@@ -33,9 +39,9 @@ def demo_chat():
     return llm
 
 
-
+# Function to fetch and process context data from a given URL
 def context_db(url):
-
+     # Loading data from the web
     loader = WebBaseLoader(url)
     data = loader.load()
     # Create embeddings using Sentence Transformers
@@ -45,20 +51,20 @@ def context_db(url):
     db.save_local(DB_FAISS_PATH)
     return embeddings
 
-    
+# Function to create a prompt template for conversation   
 def create_prompt_template(prompt_template):
     prompt = PromptTemplate(
         input_variables=["context","chat_history", "question"], template=prompt_template
     )
     return prompt
-
+# Function to initialize and configure conversation memory
 def demo_memory():
     llm_data = demo_chat()
     memory = ConversationBufferMemory(llm=llm_data,memory_key="chat_history",input_key="question",return_messages=True)
     memory.human_prefix = "user"
     memory.ai_prefix = "assistant"
     return memory
-
+# Function to filter unwanted phrases from the response
 def filter_response(response):
     unwanted_phrases = [
         "created by Mistral",
@@ -72,7 +78,7 @@ def filter_response(response):
         response = response.split("User:", 1)[1]
     return response
 
-
+# Function to conduct a conversation chain based on input text, memory, and client type
 def demo_chain(input_txt,memory,client):
     llm_data = demo_chat()
     if client == 'synapxe':
@@ -121,9 +127,10 @@ def demo_chain(input_txt,memory,client):
 
     embeddings = context_db(url)
     prompt = create_prompt_template(prompt_template)
-    # Load from local storage
+    # Loading FAISS vector store from local storage
     db = FAISS.load_local(DB_FAISS_PATH, embeddings,allow_dangerous_deserialization=True)
     retriever = db.as_retriever(search_kwargs={"k": 3})
+     # Initializing conversation chain with LL model, retriever, memory, and prompt
     llm_conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm_data,
                                                                    retriever=retriever,
                                                                    verbose=True,
@@ -131,7 +138,9 @@ def demo_chain(input_txt,memory,client):
                                                                    chain_type = "stuff",
                                                                    combine_docs_chain_kwargs={"prompt": prompt})
     history = []
+     # Running conversation chain with input text and history
     chat_reply = llm_conversation_chain.run({"question": input_txt,"chat_history": history})
+     # Filtering unwanted phrases from the response
     chat_reply = filter_response(chat_reply)
     return chat_reply
 
