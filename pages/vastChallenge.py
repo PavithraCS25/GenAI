d35457da2573967a3ce14f3d0604b1034165ad9e
PavithraCS25 <pavithrasainath7@gmail.com>
2024-04-16 07:59:58 +0000

    moved the a comment line

--- pages/vastChallenge.py
+++ pages/vastChallenge.py
@@ -23,8 +23,8 @@ with head:
 st.divider()
 # Embed Tableau visualization using HTML component
 with st.container(border=True):
-    html_code = """
     # Define HTML code to embed Tableau visualization
+    html_code = """
     <div class='tableauPlaceholder' id='viz1712836590129' style='position: relative'><noscript><a href='#'><img alt='VAST_CHALLENGE_MC2 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CY&#47;CYGT563SP&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='path' value='shared&#47;CYGT563SP' /> <param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CY&#47;CYGT563SP&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1712836590129');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
     # Embed the HTML code using the HTML component
     components.html(html_code,height=900,width=1090)
